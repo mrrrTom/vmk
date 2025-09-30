9c7d851df2f6d93e8484f8e24325d69ae9869ee9
Daniil Gorbunov <dagoroff@gmail.com>
2025-09-30 18:47:42 +0300

    task_1 10 ex

---
+++ c/task1/10/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int count = 0;
+bool check() {
+	printf("Check %d\n", count++);
+	return false;
+}
+
+int main() {
+	if (check() && check()) {
+		printf("Inside if");
+	}
+
+	return 0;
+}
+
